fd5db85de902ac6cdbf1ccd21124687699710370
Atharva Jagtap <jagtapatharva408@gmail.com>
2024-01-21 12:11:03 +0530

    new commit

--- script.js
+++ script.js
@@ -1,4 +1,3 @@
-<<<<<<< HEAD
 var tablinks = document.getElementsByClassName("tab-links");
 var tabcontents = document.getElementsByClassName("tab-contents");
 
@@ -12,18 +11,3 @@ function opentab(tabname) {
   event.currentTarget.classList.add("active-link");
   document.getElementById(tabname).classList.add("active-tab");
 }
-=======
-var tablinks = document.getElementsByClassName("tab-links");
-var tabcontents = document.getElementsByClassName("tab-contents");
-
-function opentab(tabname) {
-  for (var tablink of tablinks) {
-    tablink.classList.remove("active-link");
-  }
-  for (var tabcontent of tabcontents) {
-    tabcontent.classList.remove("active-tab");
-  }
-  event.currentTarget.classList.add("active-link");
-  document.getElementById(tabname).classList.add("active-tab");
-}
->>>>>>> 38dfce3649b1183fb23887ce543832152824e6af
